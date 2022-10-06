9f5baa5f5417996f146d90122a8eda4d4e4d56a7
karmvaghani <81067681+karmvaghani@users.noreply.github.com>
2022-10-06 16:37:58 +0530

    Add files via upload

---
+++ C Language/Ch-5/LEC 2/CH5-LEC2-3.C
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,c,d;
+	clrscr();
+
+	printf("Enter a :");
+	scanf("%d",&a);
+	printf("Enter b :");
+	scanf("%d",&b);
+	printf("Enter c :");
+	scanf("%d",&c);
+	printf("Enter d :");
+	scanf("%d",&d);
+
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is big.");
+			}
+			else
+			{
+				printf("d is big.");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is big.");
+			}
+			else
+			{
+				printf("d is big.");
+			}
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b ig big.");
+			}
+			else
+			{
+				printf("d is big.");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is big.");
+			}
+			else
+			{
+				printf("d is big.");
+			}
+		}
+	}
+
+	getch();
+
+}
